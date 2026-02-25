5acb6f1c17fba0f1f356d74fe638d3907621254d
Wagnerlucasx <daffy20235@gmail.com>
2026-02-25 14:40:34 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,22 @@
+// Main.java
+// Main.java
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Bem-vindo ao projeto IMPASSA!");
+        System.out.println("Integrantes da 11ª Classe:");
+
+        String[] alunos = {
+            "Cenilson Joaquim",
+            "Gelson Francisco",
+            "João Vissoca",
+            "Ladislau Amaral",
+            "Suzana Cabanga",
+            "João Catunda",
+            "Layne Afonso",
+            "Moisés Gonçalves",
+            "Pedro Neto",
+            "Obede Manzambe",
+            "Justina Dala",
+            "Leonardo Suekela",
+            "Jorge Celela",
+            "Valdimiro Da Costa",
